065162c3c16b429d44096c1219471c84815ba6c0
youssouf695 <youssoufjordannjupuen@gmail.com>
2026-03-30 23:43:29 +0100

    MAJ: add metadata.js || remove const metatdata on home page

---
+++ app/metadata.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Accueil - Guide Touristique IndabaX Ngaoundéré 2026',
+    description: 'Bienvenue sur le guide officieux des participants d\'IndabaX Cameroun 2026 à Ngaoundéré. Carte interactive, sites à visiter, bons plans et itinéraires.',
+    keywords: 'IndabaX Ngaoundéré, IndabaX Cameroun 2026, guide participants IndabaX, tourisme Ngaoundéré, carte interactive Ngaoundéré',
+    openGraph: {
+      title: 'Guide IndabaX 2026 - Découvrez Ngaoundéré',
+      description: 'Préparez votre séjour à Ngaoundéré pendant la conférence IndabaX Cameroun 2026',
+      url: 'https://visit-ngaoundere.vercel.app',
+    },
+  }
