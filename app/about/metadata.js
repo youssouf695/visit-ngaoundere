4a03d1b328aa919de60f524302eeb273fd51795a
youssouf695 <youssoufjordannjupuen@gmail.com>
2026-03-31 02:51:10 +0100

    Add keyword on metadata

--- app/about/metadata.js
+++ app/about/metadata.js
@@ -1,7 +1,7 @@
 export const metadata = {
     title: 'À propos - Développeur du Guide IndabaX Ngaoundéré',
     description: 'Découvrez le développeur qui a créé ce guide spécial IndabaX Cameroun 2026. Youssoufa Njupuen - Full-Stack Developer & Designer.',
-    keywords: 'développeur guide IndabaX, Youssoufa Njupuen, créateur guide Ngaoundéré, IndabaX Cameroun 2026 développeur',
+    keywords: 'développeur guide IndabaX, Youssoufa Njupuen, njupuen, youssoufa, développeur web Ngaoundéré, freelance React Cameroun, création site web Ngaoundéré, créateur guide Ngaoundéré, IndabaX Cameroun 2026 développeur',
     openGraph: {
       title: 'À propos - Créateur du Guide IndabaX Ngaoundéré',
       description: 'Youssoufa Njupuen - Le développeur derrière ce guide pour IndabaX 2026',
